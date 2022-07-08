30a86d547783577e57ba95e4395aa454dfbd33b2
Aldar <exalonv@yandex.ru>
2022-07-08 16:06:30 +0300

    authCard

--- client/src/components/AuthForm/AuthForm.js
+++ client/src/components/AuthForm/AuthForm.js
@@ -2,20 +2,20 @@ import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-
-import {regUserThunk, logOutUserThunk, logInUserThunk} from '../../redux/action/user'
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+import '../AuthForm/auth.css';
+import { regUserThunk, logOutUserThunk, logInUserThunk } from '../../redux/action/user'
 
 
 const AuthForm = () => {
   const [loginToggle, setLoginToggle] = useState(false);
   const [painterToggle, setPainterToggle] = useState(false);
-
-
-const user = useSelector((state) => state.user)
-console.log('useSelector', user)
-
-
   const [form, setForm] = useState({})
+
+  const user = useSelector((state) => state.user)
+
+
   const dispatch = useDispatch()
 
   const handleForm = () => {
@@ -26,11 +26,11 @@ console.log('useSelector', user)
   const handleSubmit = (event) => {
     event.preventDefault()
 
-    if (loginToggle && form.email && form.password){
+    if (loginToggle && form.email && form.password) {
       dispatch(logInUserThunk(form));
       setForm({})
       event.target.reset()
-      } else if (form.name && form.email && form.password) {
+    } else if (form.name && form.email && form.password) {
 
       dispatch(regUserThunk(form, role));
       setForm({})
@@ -53,57 +53,139 @@ console.log('useSelector', user)
   const role = painterToggle ? 1 : 2
 
   return (
-    <Form onSubmit={handleSubmit}>
-      <Form.Group className="mb-3" controlId="formBasicName">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="text" value={form.name || ""} name="name"
-          disabled={loginToggle} onChange={handleChange}
-          placeholder="Enter name" />
-        <Form.Text className="text-muted">
-          Enter login
-        </Form.Text>
-      </Form.Group>
+    <Card style={{ width: '100%' }}>
+      <Card.Header>Auth</Card.Header>
+      <ListGroup variant="flush">
+        <div className='formAuth'>
+          <Form onSubmit={handleSubmit}>
+            <ListGroup.Item>
+              <Form.Group className="mb-2" controlId="formBasicName">
+                <Form.Label>Name</Form.Label>
+                <Form.Control type="text" value={form.name || ""} name="name"
+                  disabled={loginToggle} onChange={handleChange}
+                  placeholder="Enter name" />
+                <Form.Text className="text-muted">
+                  Введите логин
+                </Form.Text>
+              </Form.Group>
+            </ListGroup.Item>
 
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="text" value={form.email || ""}
-          name="email"
-          onChange={handleChange} placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
+            <ListGroup.Item>
+              <Form.Group className="mb-2" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="text" value={form.email || ""}
+                  name="email"
+                  onChange={handleChange} placeholder="Enter email" />
+                <Form.Text className="text-muted">
+                  Введите почту
+                </Form.Text>
+              </Form.Group>
+            </ListGroup.Item>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" value={form.password || ""}
-          name="password"
-          onChange={handleChange} placeholder="Password" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        {loginToggle ? "Login" : "Register"}
-      </Button>
+            <ListGroup.Item>
+              <Form.Group className="mb-2" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" value={form.password || ""}
+                  name="password"
+                  onChange={handleChange} placeholder="Password" />
+                <Form.Text className="text-muted">
+                  Введите пароль
+                </Form.Text>
+              </Form.Group>
+            </ListGroup.Item>
 
-      <Form.Label>
-        <Form.Control type="checkbox" onChange={handleForm} />
-        <Form.Text className="text-muted">
-          Вы уже зарегистрированы?
-        </Form.Text>
-      </Form.Label>
 
-      <Form.Label hidden={loginToggle}>
-        <Form.Control type="checkbox" onChange={handlePainter} />
-        <Form.Text className="text-muted">
-          Зарегистрироваться как художник
-        </Form.Text>
-      </Form.Label>
-      <Button type="submit" onClick={handleLogout}>
-        Logout
-      </Button>
-    </Form>
+            <ListGroup.Item>
+              <Form.Group className="position-relative mb-2">
+                <Form.Label>Image</Form.Label>
+                <Form.Control
+                  type="file"
+                  required
+                  name="file"
+                />
+              </Form.Group>
+            </ListGroup.Item>
+
+            <ListGroup.Item>
+              <Form.Group className="mb-3" >
+                <Form.Label >
+                  Enter sex
+                </Form.Label>
+                <Form.Check
+                  type="radio"
+                  label="Male"
+                  name="sex"
+                  id="formHorizontalRadios1"
+                />
+                <Form.Check
+                  type="radio"
+                  label="Female"
+                  name="sex"
+                  id="formHorizontalRadios2"
+                />
+              </Form.Group>
+            </ListGroup.Item>
+
+            <ListGroup.Item>
+              <Form.Group className="mb-2" controlId="formBasicName">
+                <Form.Label>title</Form.Label>
+                <Form.Control type="text" name="title"
+                  placeholder="Enter title" />
+                <Form.Text className="text-muted">
+                  Введите title
+                </Form.Text>
+              </Form.Group>
+            </ListGroup.Item>
+
+            <ListGroup.Item>
+              <Form.Group className="mb-2" controlId="formBasicName">
+                <Form.Label>discription</Form.Label>
+                <Form.Control type="text" name="discription"
+                  placeholder="Enter discription" />
+                <Form.Text className="text-muted">
+                  Введите discription
+                </Form.Text>
+              </Form.Group>
+            </ListGroup.Item>
+
+            <ListGroup.Item>
+              <Form.Group className="mb-2">
+                <Form.Label>
+                  <Form.Text className="text-muted">
+                    Вы уже зарегистрированы?
+                  </Form.Text>
+                  <Form.Check aria-label="option 1" onChange={handleForm} />
+                </Form.Label>
+              </Form.Group>
+              </ListGroup.Item>
+
+              <ListGroup.Item>
+                <Form.Group className="mb-2">
+                  <Form.Label hidden={loginToggle}>
+                    <Form.Text className="text-muted">
+                      Зарегистрироваться как художник
+                    </Form.Text>
+                    <Form.Check aria-label="option 1" onChange={handlePainter} />
+                  </Form.Label>
+                </Form.Group>
+              </ListGroup.Item>
+
+              <ListGroup.Item>
+                <Form.Group className="mb-2">
+                  <Button variant="primary" type="submit">
+                    {loginToggle ? "Login" : "Register"}
+                  </Button>
+                </Form.Group>
+                <Form.Group className="mb-2">
+                  <Button type="submit" onClick={handleLogout}>
+                    Logout
+                  </Button>
+                </Form.Group>
+              </ListGroup.Item>
+          </Form>
+        </div>
+      </ListGroup>
+    </Card >
   );
 }
 
--- client/src/components/Navbar/MyNavbar.js
+++ client/src/components/Navbar/MyNavbar.js
@@ -2,7 +2,8 @@ import React from 'react'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-
+import { Link } from "react-router-dom";
+import '../Navbar/MyNavbar.css'
 
 export default function MyNavbar() {
   return (
@@ -12,9 +13,8 @@ export default function MyNavbar() {
         <Container>
           <Navbar.Brand href="">Navbar</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/auth">Auth</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Link className='link' to="/">Home </Link>
+            <Link className='link' to="/auth">/Auth</Link>
           </Nav>
         </Container>
       </Navbar>
