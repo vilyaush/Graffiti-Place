3d9dbd09d7ea8c14137897f5100f1323aa03ec41
Violetta <vilya.vishnya@gmail.com>
2022-07-08 15:49:31 +0300

    database

--- server/db/models/roles.js
+++ server/db/models/roles.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      this.belongsTo(models.Users, { foreignKey: 'roles_id' });
+      this.hasMany(models.Users, { foreignKey: 'roles_id' });
     }
   }
   Roles.init({
