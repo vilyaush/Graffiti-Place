ef5643aee4e7b5c367c3eb1f013d01109761569d
Violetta <vilya.vishnya@gmail.com>
2022-07-08 13:40:19 +0300

    routesssss

--- server/app.js
+++ server/app.js
@@ -10,7 +10,8 @@ const FileStore = require('session-file-store')(session);
 const PORT = process.env.PORT ?? 3003;
 const userRouter = require('./routes/userRouter');
 const imgRouter = require('./routes/imgRouter');
-const paintRouter = require('./routes/paintRouter')
+const paintRouter = require('./routes/paintRouter');
+const orderRouter = require('./routes/orderRouter');
 
 const app = express();
 app.use(cors({
@@ -42,7 +43,8 @@ app.use((req, res, next) => {
 });
 app.use('/user', userRouter);
 app.use('/paintercard', imgRouter);
-app.use('/create', paintRouter )
+app.use('/create', paintRouter);
+app.use('/order', orderRouter);
 
 app.use((req, res) => {
   res.status(404).send('ooops');
