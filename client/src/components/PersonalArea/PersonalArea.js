62d73fd59374486b80f0a6ef49ae0ad09e2b7ed4
Aldar <exalonv@yandex.ru>
2022-07-11 20:00:27 +0300

    cabinet

--- client/src/components/PersonalArea/PersonalArea.js
+++ client/src/components/PersonalArea/PersonalArea.js
@@ -1,15 +1,25 @@
 import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-
+import { useSelector } from 'react-redux'
+import './PersonalArea.css'
 
 const PersonalArea = () => {
 
-  const user = useSelector((state) => state.user)
-
-  console.log(user)
+  const user = useSelector((s) => s.user); // при повторном обновлении страницы личного кабинета, получаем null и страница крашится, надо пофиксить
 
   return (
-    <div>dddddddddd</div>
+    <div className='area'>
+      <ul>
+        <h2>Личный кабинет</h2>
+        <h3>Мои данные:</h3>
+        <li>Мое айди: {user.id}</li>
+        <li>Имя: {user.name}</li>
+        <li>Почта: {user.email}</li>
+        <li>Моя роль: {user.roles_id}</li>
+        <li>Аккаунт создан: {user.createdAt.slice(0,10)}</li>
+        <h3>Мои заказы</h3>
+        <li>типа заказы</li>
+      </ul>
+    </div>
   )
 }
 
