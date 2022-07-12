ebbee4d3f294ae75a305ce3611c818bb9d8c8a38
Aldar <exalonv@yandex.ru>
2022-07-12 16:42:00 +0300

    ava

--- client/src/components/PersonalArea/PersonalArea.js
+++ client/src/components/PersonalArea/PersonalArea.js
@@ -1,41 +1,40 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import './PersonalArea.css'
 import { getRolesThunk } from '../../redux/action/roles'
 import { authUserThunk } from '../../redux/action/user'
 
 const PersonalArea = () => {
+  const user = useSelector((s) => s.user); // при повторном обновлении страницы личного кабинета, получаем null и страница крашится, надо пофиксить
+  // console.log(user, 'PERSONAL_AREA')
+  const roles = useSelector((s) => s.roles);
 
-  // const dispatch = useDispatch()
+  const dispatch = useDispatch()
 
   // useEffect(() => {
   //   dispatch(authUserThunk())
   // }, [])
 
-
-  const dispatch = useDispatch();
-  const roles = useSelector((s) => s.roles);
   useEffect(() => {
     dispatch(getRolesThunk())
   }, [])
 
-  const rolesCheck = roles.filter(el => el.id === user.roles_id)
+  const rolesCheck = roles.filter((el) => el.id === user?.roles_id)
   console.log(rolesCheck);
 
 
-  const user = useSelector((s) => s.user); // при повторном обновлении страницы личного кабинета, получаем null и страница крашится, надо пофиксить
-  console.log(user, 'PERSONAL_AREA')
 
   return (
     <div className='area'>
       <ul>
         <h2>Личный кабинет</h2>
+        <li><img className='card-img' style={{ width: '200px', height: '300px' }} src={`${process.env.REACT_APP_serverApi}/img/${user?.img}`} alt='avatar' /> </li>
         <h3>Мои данные:</h3>
-        <li>Мое айди: {user.id}</li>
-        <li>Имя: {user.name}</li>
-        <li>Почта: {user.email}</li>
-        <li>Моя роль: {rolesCheck[0].roles}</li>
-        <li>Аккаунт создан: {user.createdAt.slice(0, 10)}</li>
+        <li>Мое айди: {user?.id}</li>
+        <li>Имя: {user?.name}</li>
+        <li>Почта: {user?.email}</li>
+        <li>Моя роль: {rolesCheck[0]?.roles}</li>
+        <li>Аккаунт создан: {user?.createdAt.slice(0, 10)}</li>
         <h3>Мои заказы</h3>
         <li>типа заказы</li>
       </ul>
