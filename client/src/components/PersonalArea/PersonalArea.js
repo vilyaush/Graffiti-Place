e6160dd833948d35dc03d063b9f0c27f324d5f32
Yami <Yamikoretsy@gmail.com>
2022-07-13 10:20:29 +0300

    lk

--- client/src/components/PersonalArea/PersonalArea.js
+++ client/src/components/PersonalArea/PersonalArea.js
@@ -25,38 +25,40 @@ function PersonalArea() {
 
   return (
     <div className="area">
-      <div className="areaDiv">
+      <div className="areaDiv1">
         <h1>Личный кабинет</h1>
-        <div className="areaDiv">
-          <ul>
-            <li>
-              <img className="card-img" style={{ width: '200px', height: '300px' }} src={`${process.env.REACT_APP_serverApi}/img/${user?.img}`} alt="avatar" />
-              {' '}
-            </li>
-            <h3>Мои данные:</h3>
-            <li>
-              Мое айди:
-              {user?.id}
-            </li>
-            <li>
-              Имя:
-              {user?.name}
-            </li>
-            <li>
-              Почта:
-              {user?.email}
-            </li>
-            <li>
-              Моя роль:
-              {rolesCheck[0]?.roles}
-            </li>
-            <li>
-              Аккаунт создан:
-              {user?.createdAt.slice(0, 10)}
-            </li>
-            <h3>Мои заказы</h3>
-            <li>типа заказы</li>
-          </ul>
+        <div className="obj">
+          <div className="areaPhoto">
+            <img className="card-img" style={{ width: '200px', height: '300px' }} src={`${process.env.REACT_APP_serverApi}/img/${user?.img}`} alt="avatar" />
+
+          </div>
+          <div className="areaDiv">
+            <ul>
+              <h3>Мои данные:</h3>
+              <li>
+                Мое айди:
+                {user?.id}
+              </li>
+              <li>
+                Имя:
+                {user?.name}
+              </li>
+              <li>
+                Почта:
+                {user?.email}
+              </li>
+              <li>
+                Моя роль:
+                {rolesCheck[0]?.roles}
+              </li>
+              <li>
+                Аккаунт создан:
+                {user?.createdAt.slice(0, 10)}
+              </li>
+              <h3>Мои заказы</h3>
+              <li>типа заказы</li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
