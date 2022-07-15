205a1bb4a3659183ecc7d0583ed2137bd503b71a
Aldar <exalonv@yandex.ru>
2022-07-15 10:53:09 +0300

    deploy s

--- db/database.js
+++ db/database.js
@@ -1,13 +1,11 @@
 require('dotenv').config();
 
-console.log(process.env.DB_USER);
-
 module.exports = {
   development: {
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
-    host: '127.0.0.1',
+    host: process.env.DB_HOST,
     dialect: 'postgres',
   },
   test: {
@@ -18,10 +16,13 @@ module.exports = {
     dialect: 'mysql',
   },
   production: {
-    username: 'root',
-    password: null,
-    database: 'database_production',
-    host: '127.0.0.1',
-    dialect: 'mysql',
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_HOST,
+    host: process.env.DB_NAME,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: { rejectUnauthorized: false },
+    },
   },
 };
